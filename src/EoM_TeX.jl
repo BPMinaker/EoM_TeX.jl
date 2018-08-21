1ab49d1e56e9183a5290c39998531dd0618c9e8b
BPMinaker <bminaker@uwindsor.ca>
2018-08-20 23:51:31 -0400

    v0.7

--- src/EoM_TeX.jl
+++ src/EoM_TeX.jl
@@ -6,16 +6,6 @@ import EoM
 
 export write_report
 
-# export tex_bode_pgfplot
-# export tex_bode3_pgfplot
-# export tex_eig_pgfplot
-# export tex_eig_pgftable
-# export tex_hsv_pgfplot
-# export tex_hsv_pgftable
-# export tex_sstf_pgfplot
-# export tex_sstf_pgftable
-
-
 include("write_report.jl")
 include(joinpath("tex","tex_bode_pgfplot.jl"))
 include(joinpath("tex","tex_bode3_pgfplot.jl"))
@@ -25,7 +15,5 @@ include(joinpath("tex","tex_hsv_pgfplot.jl"))
 include(joinpath("tex","tex_hsv_pgftable.jl"))
 include(joinpath("tex","tex_sstf_pgfplot.jl"))
 include(joinpath("tex","tex_sstf_pgftable.jl"))
-
-println(pwd())
 
 end
