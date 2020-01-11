087d95e5c89f4980d2fe862534559a20066b3911
BPMinaker <bminaker@gmail.com>
2020-01-11 10:09:22 -0500

    file name option

--- src/tex_time_pgfplot.jl
+++ src/tex_time_pgfplot.jl
@@ -1,4 +1,4 @@
-function tex_time_pgfplot(outs;folder="output",label="F:time",n=1:1:length(outs),caption="",skip=1)
+function tex_time_pgfplot(outs;folder="output",file="time.out",label="F:time",n=1:1:length(outs),caption="",skip=1)
 ## Copyright (C) 2019, Bruce Minaker
 ## tex_time_pgfplot.jl is free software; you can redistribute it and/or modify it
 ## under the terms of the GNU General Public License as published by
@@ -14,6 +14,8 @@ function tex_time_pgfplot(outs;folder="output",label="F:time",n=1:1:length(outs)
 
 ## Write the tex necessary to include the plots
 
+str=joinpath(folder,file)
+
 s="\\begin{figure}[htbp]\n"
 s*="\\begin{center}\n"
 s*="\\begin{footnotesize}\n"
@@ -23,7 +25,7 @@ s*="each nth point=$skip,filter discard warning=false,unbounded coords=discard,"
 s*="xlabel={Time [\\si{\\s}]},ylabel={Outputs},"
 s*="legend style={at={(1.0,1.03)},anchor=south east},legend columns=-1,cycle list name=linestyles*]\n"
 for j=1:length(n)
-	s*="\\addplot+[black,line width=1pt,mark=none] table[x index=0,y index=$(n[j])]{$folder/time.out};\n"
+	s*="\\addplot+[black,line width=1pt,mark=none] table[x index=0,y index=$(n[j])]{$str};\n"
 	s*="\\addlegendentry{\$$(outs[j])\$}\n"
 end
 s*="\\end{axis}\n"
